4d046a8f90a489f3f9dffb6fc737ac3b3aef2406
Md. Talal Wasim <md.talalwasim@gmail.com>
2023-07-15 22:50:10 +0600

    Project setup

---
+++ springboot-file-project-upload-file-directly-in-directory-folder/src/main/java/com/mdtalalwasim/fileproject2/SpringbootFileProjectUploadFileDirectlyInDirectoryFolderApplication.java
@@ -0,0 +1,13 @@
+package com.mdtalalwasim.fileproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFileProjectUploadFileDirectlyInDirectoryFolderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFileProjectUploadFileDirectlyInDirectoryFolderApplication.class, args);
+	}
+
+}
---
+++ springboot-file-project-upload-file-directly-in-directory-folder/src/test/java/com/mdtalalwasim/fileproject2/SpringbootFileProjectUploadFileDirectlyInDirectoryFolderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdtalalwasim.fileproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFileProjectUploadFileDirectlyInDirectoryFolderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
